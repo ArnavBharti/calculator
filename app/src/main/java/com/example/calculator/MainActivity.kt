b61ff47f73b312e4f5ecf18d6ae2ff8b6aa3c202
Arnav Bharti <74090983+ArnavBharti@users.noreply.github.com>
2022-12-23 21:39:10 +0530

    Created calculator class

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -7,198 +7,44 @@ import android.widget.Button
 import android.widget.TextView
 import java.util.*
 import kotlin.math.pow
+import com.example.calculator.Calculate
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         title = "Calculator by Arnav Bharti"
 
-        val textField: TextView = findViewById(R.id.mainView)
-        val outputBox: TextView = findViewById(R.id.outputBox)
-        val button: Button = findViewById(R.id.buttonDel)
-        val buttonOne: Button = findViewById(R.id.buttonOne)
-        val buttonTwo: Button = findViewById(R.id.buttonTwo)
-        val buttonThree: Button = findViewById(R.id.buttonThree)
-        val buttonFour: Button = findViewById(R.id.buttonFour)
-        val buttonFive: Button = findViewById(R.id.buttonFive)
-        val buttonSix: Button = findViewById(R.id.buttonSix)
-        val buttonSeven: Button = findViewById(R.id.buttonSeven)
-        val buttonEight: Button = findViewById(R.id.buttonEight)
-        val buttonNine: Button = findViewById(R.id.buttonNine)
-        val buttonZero: Button = findViewById(R.id.buttonZero)
-        val buttonAdd: Button = findViewById(R.id.buttonAddition)
-        val buttonSubtract: Button = findViewById(R.id.buttonSubtraction)
-        val buttonMultiply: Button = findViewById(R.id.buttonMultiplication)
-        val buttonDivide: Button = findViewById(R.id.buttonDivision)
-        val buttonPower: Button = findViewById(R.id.buttonPower)
-        val buttonDecimal: Button = findViewById(R.id.buttonDecimal)
-        val buttonEquals: Button = findViewById(R.id.buttonEqualsSign)
-        val buttonOpenParenthesis: Button = findViewById(R.id.buttonOpenParenthesis)
-        val buttonCloseParenthesis: Button = findViewById(R.id.buttonCloseParenthesis)
-        val buttonAllClear: Button = findViewById(R.id.buttonAllClear)
-
-        button.setOnClickListener { textField.text = "${textField.text}".dropLast(1) }
-        buttonOne.setOnClickListener { textField.text = "${textField.text}1" }
-        buttonTwo.setOnClickListener { textField.text = "${textField.text}2" }
-        buttonThree.setOnClickListener { textField.text = "${textField.text}3" }
-        buttonFour.setOnClickListener { textField.text = "${textField.text}4" }
-        buttonFive.setOnClickListener { textField.text = "${textField.text}5" }
-        buttonSix.setOnClickListener { textField.text = "${textField.text}6" }
-        buttonSeven.setOnClickListener { textField.text = "${textField.text}7" }
-        buttonEight.setOnClickListener { textField.text = "${textField.text}8" }
-        buttonNine.setOnClickListener { textField.text = "${textField.text}9" }
-        buttonZero.setOnClickListener { textField.text = "${textField.text}0" }
-        buttonAdd.setOnClickListener { textField.text = "${textField.text}+" }
-        buttonSubtract.setOnClickListener { textField.text = "${textField.text}-" }
-        buttonMultiply.setOnClickListener { textField.text = "${textField.text}*" }
-        buttonDivide.setOnClickListener { textField.text = "${textField.text}/" }
-        buttonPower.setOnClickListener { textField.text = "${textField.text}^" }
-        buttonEquals.setOnClickListener {
-            outputBox.text = calculate(textField.text.toString())
-        }
-        buttonOpenParenthesis.setOnClickListener { textField.text = "${textField.text}(" }
-        buttonCloseParenthesis.setOnClickListener { textField.text = "${textField.text})" }
-        buttonDecimal.setOnClickListener { textField.text = "${textField.text}." }
-        buttonAllClear.setOnClickListener { textField.text = "" }
+        binding.buttonDel.setOnClickListener { binding.mainView.text = "${binding.mainView.text}".dropLast(1) }
+        binding.buttonOne.setOnClickListener { binding.mainView.text = "${binding.mainView.text}1" }
+        binding.buttonTwo.setOnClickListener { binding.mainView.text = "${binding.mainView.text}2" }
+        binding.buttonThree.setOnClickListener { binding.mainView.text = "${binding.mainView.text}3" }
+        binding.buttonFour.setOnClickListener { binding.mainView.text = "${binding.mainView.text}4" }
+        binding.buttonFive.setOnClickListener { binding.mainView.text = "${binding.mainView.text}5" }
+        binding.buttonSix.setOnClickListener { binding.mainView.text = "${binding.mainView.text}6" }
+        binding.buttonSeven.setOnClickListener { binding.mainView.text = "${binding.mainView.text}7" }
+        binding.buttonEight.setOnClickListener { binding.mainView.text = "${binding.mainView.text}8" }
+        binding.buttonNine.setOnClickListener { binding.mainView.text = "${binding.mainView.text}9" }
+        binding.buttonZero.setOnClickListener { binding.mainView.text = "${binding.mainView.text}0" }
+        binding.buttonAddition.setOnClickListener { binding.mainView.text = "${binding.mainView.text}+" }
+        binding.buttonSubtraction.setOnClickListener { binding.mainView.text = "${binding.mainView.text}-" }
+        binding.buttonMultiplication.setOnClickListener { binding.mainView.text = "${binding.mainView.text}*" }
+        binding.buttonDivision.setOnClickListener { binding.mainView.text = "${binding.mainView.text}/" }
+        binding.buttonPower.setOnClickListener { binding.mainView.text = "${binding.mainView.text}^" }
+        binding.buttonEqualsSign.setOnClickListener { binding.outputBox.text = Calculate().calculate(binding.mainView.text.toString()) }
+        binding.buttonOpenParenthesis.setOnClickListener { binding.mainView.text = "${binding.mainView.text}(" }
+        binding.buttonCloseParenthesis.setOnClickListener { binding.mainView.text = "${binding.mainView.text})" }
+        binding.buttonDecimal.setOnClickListener { binding.mainView.text = "${binding.mainView.text}." }
+        binding.buttonAllClear.setOnClickListener { binding.mainView.text = ""; binding.outputBox.text = "" }
 
     }
 
-    private fun performOperation(operands: Stack<Double>, operations: Stack<Char>): Double? {
-        val a: Double = operands.pop()
-        val b: Double = operands.pop()
 
-        if (a == 0.0 && operations.peek() == '/') {
-            return null
-        }
-
-        when (operations.pop()) {
-            '+' -> return (a + b)
-            '-' -> return (b - a)
-            '*' -> return (a * b)
-            '/' -> return (b / a)
-            '^' -> return b.pow(a)
-        }
-
-        return null
-    }
-
-    private fun precedence(c: Char): Int {
-        when (c) {
-            '+', '-' -> return 1
-            '*', '/' -> return 2
-            '^' -> return 3
-        }
-        return -1
-    }
-
-    private fun isOperator(c: Char): Boolean {
-        return c == '+' || c == '-' || c == '/' || c == '*' || c == '^'
-    }
-
-    private fun calculate(exp: String): String {
-        try {
-            val operands: Stack<Double> = Stack()
-            val operations: Stack<Char> = Stack()
-            var i = 0
-            while (i < exp.length) {
-                var c = exp[i]
-                if (Character.isDigit(c) || c == '.') {
-                    var num = ""
-                    while (Character.isDigit(c) || c == '.') {
-                        if (c == '.') {
-                            num += "."
-                        } else {
-                            num += c
-                        }
-                        i++
-                        c = if (i < exp.length) {
-                            exp[i]
-                        } else {
-                            break
-                        }
-                    }
-                    i--
-                    operands.push(num.toDouble())
-                } else if (c == '(') {
-                    operations.push(c)
-                } else if (c == ')') {
-                    while (!operations.isEmpty() && operations.peek() != '(') {
-                        val output = performOperation(operands, operations)
-                        operands.push(output)
-                    }
-                    operations.pop()
-                } else if (isOperator(c)) {
-                    if (c == '-' && i==0) {
-                        var num = "-"
-                        i++
-                        if (i < exp.length) {
-                            c = exp[i]
-                        }
-                        while ((Character.isDigit(c) || c == '.') || c == '-' && isOperator(exp[i-1])) {
-                            if (c == '.') {
-                                num += "."
-                            } else {
-                                num += c
-                            }
-                            i++
-                            c = if (i < exp.length) {
-                                exp[i]
-                            } else {
-                                break
-                            }
-                        }
-                        i--
-                        operands.push(num.toDouble())
-                    } else if ((c == '-' && isOperator(exp[i-1])) || (c == '-' && exp[i-1] in listOf('(',')'))) {
-                        var num = "-"
-                        i++
-                        if (i < exp.length) {
-                            c = exp[i]
-                        }
-                        while (Character.isDigit(c) || c == '.') {
-                            if (c == '.') {
-                                num += "."
-                            } else {
-                                num += c
-                            }
-                            i++
-                            c = if (i < exp.length) {
-                                exp[i]
-                            } else {
-                                break
-                            }
-                        }
-                        i--
-                        operands.push(num.toDouble())
-                    } else {
-
-                        while (!operations.isEmpty() && precedence(c) <= precedence(operations.peek())) {
-                            val output = performOperation(operands, operations)
-                            operands.push(output)
-                        }
-                        operations.push(c)
-                    }
-
-                }
-                i++
-            }
-            while (!operations.isEmpty() && operands.size >= 1) {
-                val output = performOperation(operands, operations)
-                operands.push(output)
-            }
-            return (operands.pop().toString())
-        } catch (e: EmptyStackException) {
-            return ("Error")
-        } catch (e: java.lang.NullPointerException) {
-            return ("Error")
-        } catch (e: NotImplementedError) {
-            return "Error"
-        } catch (e: NumberFormatException) {
-            return "Error"
-        }
-    }
 
 }
